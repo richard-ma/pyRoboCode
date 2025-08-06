5d226f692c6621f5a1eb8bdaaa9756b4c43c557a
richard-ma <richard.ma.19850509@gmail.com>
2025-08-06 22:45:12 +0800

    change window title

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pygame
 if __name__ == "__main__":
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption("Pygame Window")
+    pygame.display.set_caption("Python RoboCode")
 
     running = True
     while running:

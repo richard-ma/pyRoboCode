c2b595f384b7bcdf66361f2ed15a035fd858f53b
richard-ma <richard.ma.19850509@gmail.com>
2025-08-06 22:44:44 +0800

    add pygame basic window

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Pygame Window")
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # Check for window close
+                running = False
+            if event.type == pygame.KEYDOWN: # Check for key press
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
+        screen.fill((0, 0, 0))  # Fill the screen with black
+        pygame.display.flip()
+
+    pygame.quit()

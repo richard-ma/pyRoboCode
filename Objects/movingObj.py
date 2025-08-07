4d330e75a3aa4efa7d1f0e581580080c04031fd8
richard-ma <richard.ma.19850509@gmail.com>
2025-08-07 21:05:55 +0800

    add move stop turn and lockTurn methods

--- Objects/movingObj.py
+++ Objects/movingObj.py
@@ -7,6 +7,7 @@ class MovingObj:
         self.isMoving = False
         self.position = Vector2(0, 0)
         self.angle = 0
+        self.isLocked = False
 
     def getNextPos(self, unit=10.0):
         if self.isMoving:
@@ -14,3 +15,18 @@ class MovingObj:
             dy = - math.cos(math.radians(self.angle)) * unit # y axis is inverted in pygame
             self.position = Vector2(self.position.x + dx, self.position.y + dy)
         return self.position
+
+    def move(self):
+        self.isMoving = True
+
+    def stop(self):
+        self.isMoving = False
+
+    def turn(self, angle):
+        if not self.isLocked:
+            # Normalize the angle to be within 0-360 degrees
+            self.angle = (self.angle + angle) % 360
+        return self.angle
+        
+    def lockTurn(self):
+        self.isLocked = True

bc902ec37f5748cf0f7ac9926cf60dacdc1e9935
richard-ma <richard.ma.19850509@gmail.com>
2025-08-07 20:43:37 +0800

    clear files

--- Objects/bullet.py
+++ Objects/bullet.py
@@ -22,32 +22,3 @@ class Bullet(MovingObj):
         bsize = max(4, min(bsize, 10)) # 4 <= bsize <= 10
         self.bsize = bsize
         
-    # def setColour(self, color):
-    #     mask = self.pixmap.createMaskFromColor(self.maskColor,  1)
-    #     p = QPainter(self.pixmap)
-    #     p.setPen(color)
-    #     p.drawPixmap(self.pixmap.rect(), mask, mask.rect())
-    #     p.end()
-    #     self.setPixmap(self.pixmap)
-    #     self.maskColor = color
-        
-    # def advance(self):
-    #     if self.isfired:
-    #         pos = self.pos
-    #         x, y = pos.x, pos.y
-    #         dx = - math.sin(math.radians(self.angle))*10.0
-    #         dy = math.cos(math.radians(self.angle))*10.0
-    #         self.setPos(x+dx, y+dy)
-    #         if x < 0 or y < 0 or x > self.scene.width or y > self.scene.height:
-    #             self.robot.onBulletMiss(id(self))
-    #             self.scene.removeItem(self)
-    #             self.robot.removeMyProtectedItem(self)
-
-        
-            
-            
-            
-            
-            
-            
-            
